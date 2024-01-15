6a492a4feec7b81305e6ec9cd63e278d555e5385
Dakhche101 <hamza.dakhche@gmail.com>
2024-01-15 22:18:25 +0100

    modification on migraiton

--- database/migrations/2024_01_15_210515_create_todos_table.php
+++ database/migrations/2024_01_15_210515_create_todos_table.php
@@ -13,7 +13,6 @@ return new class extends Migration
     {
         Schema::create('todos', function (Blueprint $table) {
             $table->id();
-            $table->id();
             $table->string('title');
             $table->boolean('completed')->default(false);
             $table->timestamps();
